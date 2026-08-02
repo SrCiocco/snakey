edf38f078e3feede89d9323bd1a867d270e1d07f
Marcos Ojeda <193316448+SrCiocco@users.noreply.github.com>
2026-08-02 14:23:48 -0300

    feat(render): add player icon and tickrate movement (delta time accumulator)

--- src/main.c
+++ src/main.c
@@ -4,15 +4,19 @@
 #define WIDTH  800
 #define HEIGHT 600
 #define FONT_SIZE 20
+#define GRID_SIZE 12
+#define TICK_RATE 0.15f /* 150ms to wait before moving */
 
 typedef struct Player {
 	Vector2 position;
 	Vector2 direction;
+	Rectangle icon;
 } Player;
 
 void handle_input(Player *player);
-void update_player(Player *player);
+void update_player(Player *player, float deltaTime, float *moveTimer);
 void draw_debug_info(const Player *player);
+void draw_player(Player *player);
 
 int main(void)
 {
@@ -24,18 +28,23 @@ int main(void)
 
 	Player player = {
 		.position = {(width / 2.0f), (height / 2.0f)},
-		.direction = {0, 0}
+		.direction = {0, 0},
+		.icon = {player.position.x, player.position.y, GRID_SIZE, GRID_SIZE}
 	};
 
 	if (!IsWindowReady())
 		return -1;
 
+	float moveTimer = 0.0f;
+
 	while(!WindowShouldClose()) {
+		float deltaTime = GetFrameTime();
 		handle_input(&player);
-		update_player(&player);
+		update_player(&player, deltaTime, &moveTimer);
 		BeginDrawing();
 		ClearBackground(WHITE);
 		draw_debug_info(&player);
+		draw_player(&player);
 		EndDrawing();
 	}
 
@@ -65,9 +74,24 @@ void draw_debug_info(const Player *player)
 	DrawText(TextFormat("Direction: %f, %f", player->direction.x, player->direction.y), 0, 20, FONT_SIZE, RED);
 }
 
-void update_player(Player *player)
+void update_player(Player *player, float deltaTime, float *moveTimer)
 {
-	player->position.x += player->direction.x;
-	player->position.y += player->direction.y;
-	
+	*moveTimer += deltaTime;
+
+	if (*moveTimer >= TICK_RATE) {
+		player->position.x += player->direction.x * GRID_SIZE;
+		player->position.y += player->direction.y * GRID_SIZE;
+
+		*moveTimer -= TICK_RATE;
+	}
+
+
+}
+
+void draw_player(Player *player)
+{
+	player->icon.x = player->position.x;
+	player->icon.y = player->position.y;
+
+	DrawRectangleRec(player->icon, RED);
 }
